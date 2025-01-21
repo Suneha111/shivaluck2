b9b864c1b4507a3151306841ffd33d1fcc7ae658
Suneha111 <23f1002574@ds.study.iitm.ac.in>
2025-01-21 21:28:49 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -23,6 +23,7 @@ class handler(BaseHTTPRequestHandler):
         response = {"marks": marks}
 
         # Send the HTTP response headers
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
